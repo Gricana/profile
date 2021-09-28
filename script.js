47e1675865d3197ccce6de5b9f9e62d92aee2cfa
mas-gae76 <alex2904.goryachev@yandex.com>
2021-09-28 21:59:47 +0300

    add code to rotate the block in different directions

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ function showMenu() {
     $('.submenu_projects').slideDown(600);
 }
 function rotate() {
-    $('.cube:hover').css('transform', 'rotateX(0deg) rotateY(-90deg) translateZ(-40px)');
+    if (!($('.steps .wrap:last-child .cube:hover').length <= 0)) {
+        $('.cube:hover').css('transform', 'rotateX(0deg) rotateY(90deg) translateZ(-50px)');
+    } else {
+          $('.cube:hover').css('transform', 'rotateX(0deg) rotateY(-90deg) translateZ(-50px)');
+    }
 }
 function rotateBack() {
     $('.cube:hover').css('transform', 'rotateX(0deg) rotateY(0deg)');
@@ -16,7 +20,7 @@ $(document).ready(function () {
     $('.arrow_detail').on('click', rotate);
     $('.right').on('click', rotateBack);
 
-    $(window).scroll(function () {
+    $(window).scroll(() => {
         if ($(this).scrollTop() > 100) {
             $('.arrow_up').fadeIn();
         } else {
@@ -24,7 +28,7 @@ $(document).ready(function () {
         }
     });
 
-    $('.arrow_up').click(function () {
+    $('.arrow_up').click(() => {
         $('html, body').animate({scrollTop: 0}, 600);
         return false;
     });
